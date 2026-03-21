99ff4b6134c2db8c2818dcf74e9709dd67b9dd64
SuFxGIT <SuFxGIT@localhost>
2026-03-21 12:04:13 +0300

    chore: delete unused frontend services barrel index

--- frontend/src/services/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Centralized exports for all API services
- */
-export { default as apiClient } from './apiClient';
-export { configService } from './configService';
-export { statsService } from './statsService';
-export { schedulerService } from './schedulerService';
-export { fetchMediaLibrary, searchMedia } from './mediaLibraryService';
-export { notificationService } from './notificationService';
